bd9fb20783874b72c5d9167e0ac4d58acbb5a9c8
Holly Ford <holly_ford@hotmail.com>
2021-11-12 21:23:31 +0000

    added colour and ascii art to command line app

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const { Animal } = require("./classes");
 const { initQs } = require("./initQs");
 const inquirer = require('inquirer');
+const chalk = require('chalk');
+const figlet = require('figlet');
+
+
 
 let pet
 
@@ -13,43 +17,81 @@ inquirer
     .then(() => gameLoop())
 
 const gameLoop = () => {
-    console.log(`You have chosen a ${pet.typeOfPet} and called them ${pet.petName}`)
+    if (pet.typeOfPet === "Cat") {
+        figlet(`CAT!!`, function (err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+            console.log(data)
+        });
+    } else if (pet.typeOfPet === "Dog") {
+        figlet('DOG!!', function (err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+            console.log(data)
+        });
+    } else if (pet.typeOfPet === "Snake") {
+        figlet('SNAKE!!', function (err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+            console.log(data)
+        });
+    } else if (pet.typeOfPet === "Rabbit") {
+        figlet('RABBIT!!', function (err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+            console.log(data)
+        });
+    } 
+    console.log(chalk.green.bold(`Stats for your ${pet.typeOfPet}, ${pet.petName}:`))
     console.table(pet)
     if (pet.hunger <= 0) {
-        console.log(`${pet.petName} has died of hunger`)
+        console.log(chalk.red.bold(`${pet.petName} has died of hunger`))
         return
     } else if (pet
         .thirst <= 0) {
-        console.log(`${pet.petName} has died of thirst`)
+        console.log(chalk.red.bold(`${pet.petName} has died of thirst`))
         return
     } else if (pet.happiness <= 0) {
-        console.log(`${pet.petName} has died of sadness`)
+        console.log(chalk.red.bold(`${pet.petName} has died of sadness`))
         return
-    } else if (pet.clean <= 0) {
-        console.log(`${pet.petName} has died because they were soooo dirty!`)
+    } else if (pet.cleanliness <= 0) {
+        console.log(chalk.red.bold(`${pet.petName} has died because they were soooo dirty!`))
         return
     }
     inquirer
-        .prompt([
-            {
-                type: "rawlist",
-                name: "action",
-                message: "What would you like to do?",
-                choices: ["Feed", "Water", "Play", "Wash"],
-            },
-        ])
-        .then((answers) => {
-            if (answers.action === "Feed") {
-                pet.feed()
-            } else if (answers.action === "Water") {
-                pet.water()
-            } else if (answers.action === "Play") {
-                pet.play()
-            } else if (answers.action === "Wash") {
-                pet.wash()
-            } 
-            
-            console.table(pet)
-        })
-        .then(() => gameLoop())
+            .prompt([
+                {
+                    type: "list",
+                    name: "action",
+                    message: "What would you like to do?",
+                    choices: ["Feed", "Water", "Play", "Wash"],
+                },
+            ])
+            .then((answers) => {
+                if (answers.action === "Feed") {
+                    pet.feed()
+                } else if (answers.action === "Water") {
+                    pet.water()
+                } else if (answers.action === "Play") {
+                    pet.play()
+                } else if (answers.action === "Wash") {
+                    pet.wash()
+                }
+
+                console.table(pet)
+
+            })
+            .then(() => gameLoop())
 }
--- classes.js
+++ classes.js
@@ -5,28 +5,28 @@ class Animal {
         this.hunger = 10,
         this.thirst = 10,
         this.happiness = 10,
-        this.clean = 10
+        this.cleanliness = 10
     }
     feed(){
         this.hunger += 5
         this.thirst -= 1
         this.happiness -= 1
-        this.clean -= 1
+        this.cleanliness -= 1
     }
     water(){
         this.thirst += 5
         this.hunger -= 1
         this.happiness -= 1
-        this.clean -= 1
+        this.cleanliness -= 1
         }
     play(){
         this.thirst -= 1
         this.hunger -= 1
         this.happiness += 5
-        this.clean -= 1
+        this.cleanliness -= 1
     }
     wash(){
-        this.clean += 5
+        this.cleanliness += 5
         this.thirst -= 1
         this.happiness -= 1
         this.hunger -= 1
